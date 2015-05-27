403544acf8a84487fe78d0772792ae6f886ee2c7
Magda Kosior <magdakosior@gmail.com>
2015-05-27 09:07:52 -0600

    Adds getExports action
    returns signed url of contents of requested bucket.  Still need to
    think about deleting expired urls.

--- index.js
+++ index.js
@@ -4,66 +4,44 @@ var pgp = require('pg-promise')();
 var admZip = require('adm-zip');
 var Q = require('q');
 var _ = require('underscore');
+var PGExport = require('./lib/pg-export');
 
 var RDSIngress = require('./lib/rds-ingress');
 
 exports.handler = function (event, context) {
-    var ingressOptions = {
-        DBSecurityGroupName: event.rds.securityGroup
+
+    var options = {
+        bucket: 'pgexport-awslambda',
+        rds: event.rds,
+        folder: event.folder
+    };
+    var pgExport = new PGExport(options);
+
+    var actions = {
+        exportData: function () {
+            return pgExport.exportData(event.queries);
+        },
+        getExports: function (event) {
+            return pgExport.getExports();
+        },
+        getExportSignedUrl: function (event) {
+            // aws s3 get signed url
+            var options = {
+                bucket: 'pgexport-awslambda',
+                key: event.key
+            };
+            // var s3 = new AWS.S3();
+            // var params = {Bucket: 'myBucket', Key: 'myKey'};
+            // s3.getSignedUrl('getObject', params, function (err, url) {
+            //   console.log("The URL is", url);
+            // });
+        }
     };
 
-    var db = pgp(event.pgurl);
-    var rdsIngress = new RDSIngress(ingressOptions);
-
-    function csvExport(filename, query){
-        console.log('querying database with: ', query);
-        return db.query(query, undefined, queryResult.many)
-            .then(function (rows) {
-                console.log('query returned for %s with %s rows', query, rows.length);
-                var csv = rows.reduce(function (memo, row) { return [memo, row.result].join('\n') }, '');
-                return {
-                    filename: filename,
-                    data: csv
-                };
-            }, function (err) { console.log(err) });
-    }
-
-    rdsIngress.authorizeMe()
-        .then(function () {
-            var csvExports = event.queries.map(function (query) {
-                return csvExport(query.filename, query.sql);
-            });
-            return Q.allSettled(csvExports);
+    actions[event.action](event)
+        .then(function (result) {
+            console.log(event.action + ' succeeded ')
+            context.done(null, result);
         })
-        .then(function (results) {
-            console.log('zipping %s files', results.length);
-            results = _.groupBy(results, 'state');
-            var zip = results.fulfilled.reduce(function (zip, row) {
-                if (row.value != null)
-                    zip.addFile(row.value.filename, row.value.data);
-                return zip;
-            }, new admZip());
-
-            var params = {
-                Bucket: event.bucket,
-                Key: event.key,
-                ContentType: 'application/zip',
-                Body: zip.toBuffer()
-            };
-            var s3 = new aws.S3();
-
-            console.log('uploading zip file to s3');
-            return Q.nbind(s3.upload, s3)(params);
-        })
-        .then(rdsIngress.revoke.bind(rdsIngress))
-        .then(function () {
-            context.done(null, 'Successfully exported data.');
-        })
-        .fail(function (e) {
-            console.log('error in chain: ', e);
-            rdsIngress.revoke()
-                .then(function () {
-                    context.done(e);
-                });
-        });
+        .fail(context.done);
 };
--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 var lambda = require('./index.js');
 
 var evt = {
-    pgurl: "postgres://afgo:YUaXqGc37ANy@tsi-postgres-1.cn0lfmewpatf.us-east-1.rds.amazonaws.com/afgo_dev",
+    action: "getExports",
+    folder: "magda",
     rds:
         {
-            securityGroup : 'pg-lambda'
+            pgurl: "postgres://afgo:YUaXqGc37ANy@tsi-postgres-1.cn0lfmewpatf.us-east-1.rds.amazonaws.com/afgo_dev",
+            securitygroup : 'pg-lambda'
         },
     queries: [
         {
             filename: 'plot.csv',
             sql: "SELECT * FROM app.export_report_as_csv('app.report_full','APLY')"//app.export_as_csv('psp.plot', 'APLY')"
         }
-    ],
-    bucket: "pgexport-awslambda",
-    key: "9d4e9ddf-28cb-45a1-8e17-748694831155.zip"
+    ]
 };
 
 var context = {
     done: function(err, data) {
-        if(err) console.log('lambda exited with errors:', err)
-        else console.log('lambda exited without errors', data)
+        if(err) console.log('lambda exited with errors: ', err)
+        else console.log('lambda exited without errors ', data)
     }
 };
 

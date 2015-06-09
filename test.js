48e83afde1694488803287ebdb4c20cf3368d322
Boris Cosic <boris.cosic@tesera.com>
2015-06-09 11:14:24 -0600

    Cleaned up put, hooked up getObject and getSignedUrl.

--- test.js
+++ test.js
@@ -1,36 +1,25 @@
 var lambda = require('./index.js');
 require('node-env-file')('.env');
 
-//var evt = {
-//    "action": "exportData",
-//    "key": "magda/reports/bc9da3a5-9dcc-401b-846d-9f060fc43ce9/full.zip",
-//    "pgurl": process.env.PGURL,
-//    "rds": {
-//      "DBSecurityGroupName": "pg-lambda"
-//    },
-//    "queries": [
-//      {
-//        "filename": "report_full.csv",
-//        "sql": "SELECT * FROM app.export_report_as_csv('app.report_full', 'APLY')"
-//      }
-//    ]
-//};
-
- var evt = {
-     "action": "getExports",
-     "key": "magda/reports",
- };
-
 // var evt = {
-//     action: "getExportSignedUrl",
-//     folder: "magda",
-    // "pgurl": process.env.PGURL,
-//     rds:
-//         {
-//             securitygroup : 'pg-lambda'
-//         },
-//     url: "ba8f9215-65e3-46d8-8fde-10457ead6719.zip"
+//     "action": "exportData",
+//     "key": "magda/reports/ad9da3a5-9dcc-401b-846d-9f060fc43ce9/full.zip",
+//     "pgurl": process.env.PGURL,
+//     "rds": {
+//       "DBSecurityGroupName": "pg-lambda"
+//     },
+//     "queries": [
+//       {
+//         "filename": "report_full.csv",
+//         "sql": "SELECT * FROM app.export_report_as_csv('app.report_full')"
+//       }
+//     ]
 // };
+
+var evt = {
+ "action": "getExports",
+ "key": "magda/reports",
+};
 
 var context = {
     done: function(err, data) {
